c981ac3df5bc3806ba9b952eab961483d1717583
Abhyasa Danu B <abhyasadanub@gmail.com>
2023-11-14 18:11:38 +0700

    fix client

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
-"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthContextProvider } from "@/context/AuthContext";
-import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
