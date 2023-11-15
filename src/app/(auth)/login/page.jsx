97e9a85ad2c973c1ca69baac2c5f1aac1b62c4ba
Abhyasa Danu B <abhyasadanub@gmail.com>
2023-11-15 16:54:31 +0700

    fix some responsive

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,13 +1,13 @@
 "use client";
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import signIn from "@/firebase/auth/signin";
-import { useRouter } from "next/navigation";
 import { getRedirectResult, signInWithRedirect } from "firebase/auth";
 import { auth, provider } from "@/firebase/config";
+import { useRouter } from "next/navigation";
 
 function Page() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
   useEffect(() => {
--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,5 +1,4 @@
 import { customInitApp } from "@/firebase/firebase-admin-config";
-import { auth } from "firebase-admin";
 import { cookies, headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
