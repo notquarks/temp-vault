acbe9672668d92730f4a490995fde01eda8bf61c
Abhyasa Danu B <abhyasadanub@gmail.com>
2023-11-19 16:30:50 +0700

    fix file view for audio

--- src/app/(root)/[fileId]/components/AudioView.jsx
+++ src/app/(root)/[fileId]/components/AudioView.jsx
@@ -7,11 +7,13 @@ import Actions from "../../components/actions";
 export default function AudioView({ data }) {
   return (
     <div className="relative flex flex-col h-fit aspect-video justify-center items-center">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 justify-center items-center">
         <p>{data.fileName}</p>
-        <ReactAudioPlayer src={data.downloadUrl} controls />
-        <div className="flex flex-row gap-2">
-          <Actions data={data} />
+        <div>
+          <ReactAudioPlayer src={data.downloadUrl} controls />
+          <div className="flex flex-row gap-2">
+            <Actions data={data} />
+          </div>
         </div>
       </div>
     </div>
