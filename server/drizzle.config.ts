d9bd322b5899a198e08d78fd0f60425e5c5cc3eb
notquarks <notquarks@gmail.com>
2026-06-26 15:03:10 +0700

    more fixes

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+import { join } from "path";
+
+config({ path: join(__dirname, "../.env") });
 
 export default defineConfig({
-  dialect: "sqlite",
+  dialect: "turso",
   schema: "./db/schema.ts",
   dbCredentials: {
-    url: "./data/main.db",
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [cloudflare(), tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
